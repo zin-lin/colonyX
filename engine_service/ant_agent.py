3cbabac1f9ba1aec351eea3ce3fd550380b9996a
Damian James <zinlinhtun34@gmail.com>
2024-11-21 05:57:19 +0000

    Added grid

---
+++ engine_service/ant_agent.py
@@ -0,0 +1,23 @@
+# Author : Zin Lin Htun
+# agent class
+
+class Agent:
+    # Constructor
+    def __init__(self, coord):
+        self.coord = coord
+
+    # move around
+    def move(self):
+        return
+
+    # scan around environment
+    def scan(self):
+        return
+
+    # attack other agents
+    def attack(self):
+        return
+
+    # leave pheromone trial
+    def leave_pheromone(self):
+        return
---
+++ engine_service/coordinate.py
@@ -0,0 +1,48 @@
+# Author : Zin Lin Htun
+# Coordinate class
+
+
+class Coordinate:
+    x = 0
+    y = 0
+
+    # Constructor
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    # move left
+    def move_left(self, x=1):
+        self.x -= x
+
+    # move right
+    def move_right(self, x=1):
+        self.x += x
+
+    # move up
+    def move_up(self, y=1):
+        self.y -= y
+
+    # move down
+    def move_down(self, y=1):
+        self.y += y
+
+    # move left and up
+    def move_left_up(self, x=1, y=1):
+        self.x -= x
+        self.y += y
+
+    # move right and up
+    def move_right_up(self, x=1, y=1):
+        self.x += x
+        self.y += y
+
+    # move left and down
+    def move_left_down(self, x=1, y=1):
+        self.x -= x
+        self.y -= y
+
+    # move right and down
+    def move_right_down(self, x=1, y=1):
+        self.x += x
+        self.y -= y
